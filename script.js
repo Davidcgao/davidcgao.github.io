954780f608f783b8dbd4f396205947461ef6d4e1
Davidcgao <davidgao22@gmail.com>
2017-04-30 20:33:02 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+(function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
+
 
 
  function newpage() {
@@ -33,19 +35,6 @@ function playAudio() {
   $audio.attr('src', singleAudio[index]);
   $audio.get(0).play();
 }
-
-//expand contract thumbs
-//has something to do with clearqueue(queuename)
-//clearing queue of all .innerqueue is causing it to double flash
-
-//function expand () {   
-//    $(this).clearQueue();
-//    $('.innerText').clearQueue();
-//    $(this).animate({'wi dth': '36em'}, 900);
-//    $(this).animate({'height': '10em'}, 500);
-////    $(this).animate({'width': '36em'}, 500);
-//    $('.innerText').delay(1000).animate({'opacity': '0.5'}, 500);
-//}
 
 function graphicdesignExpand () {   
     playAudio();
@@ -144,65 +133,29 @@ function otherExpand () {
 function graphicdesignContract () {
     $(this).clearQueue();
     $('.innerText-graphicdesign').clearQueue().animate({'opacity': '0.0'}, 300);
-    $(this).delay( 500 ).animate({'height': '1em'}, 500);
+    $(this).delay( 500 ).animate({'height': '10em'}, 500);
     $(this).animate({'width': '10em'}, 500);                           
-}
-
-function floatContract () {
-    $(this).clearQueue();
-    $('.innerText-float').clearQueue().animate({'opacity': '0.0'}, 300);
-    $(this).delay( 500 ).animate({'height': '1em'}, 500);
-    $(this).animate({'width': '11em'}, 500);                           
 }
 
 function hiveContract () {
     $(this).clearQueue();
     $('.innerText-hive').clearQueue().animate({'opacity': '0.0'}, 300);
-    $(this).delay( 500 ).animate({'height': '1em'}, 500);
+    $(this).delay( 500 ).animate({'height': '10em'}, 500);
     $(this).animate({'width': '10em'}, 500);                           
 }
 function noisyContract () {
     $(this).clearQueue();
     $('.innerText-noisy').clearQueue().animate({'opacity': '0.0'}, 300);
-    $(this).delay( 500 ).animate({'height': '1em'}, 500);
-    $(this).animate({'width': '12em'}, 500);                           
-}
-function mapContract () {
-    $(this).clearQueue();
-    $('.innerText-map').clearQueue().animate({'opacity': '0.0'}, 300);
-    $(this).delay( 500 ).animate({'height': '1em'}, 500);
-    $(this).animate({'width': '9em'}, 500);                           
-}
-function darkroomContract () {
-    $(this).clearQueue();
-    $('.innerText-darkroom').clearQueue().animate({'opacity': '0.0'}, 300);
-    $(this).delay( 500 ).animate({'height': '1em'}, 500);
-    $(this).animate({'width': '13em'}, 500);                           
+    $(this).delay( 500 ).animate({'height': '10em'}, 500);
+    $(this).animate({'width': '10em'}, 500);                           
 }
 function surfaceContract () {
     $(this).clearQueue();
     $('.innerText-surface').clearQueue().animate({'opacity': '0.0'}, 300);
-    $(this).delay( 500 ).animate({'height': '1em'}, 500);
-    $(this).animate({'width': '14em'}, 500);                           
-}
-function screwsContract () {
-    $(this).clearQueue();
-    $('.innerText-screws').clearQueue().animate({'opacity': '0.0'}, 300);
-    $(this).delay( 500 ).animate({'height': '1em'}, 500);
-    $(this).animate({'width': '12em'}, 500);                           
-}
-function othergameContract () {
-    $(this).clearQueue();
-    $('.innerText-').clearQueue().animate({'opacity': '0.0'}, 300);
-    $(this).delay( 500 ).animate({'height': '1em'}, 500);
-    $(this).animate({'width': '9em'}, 500);                           
-}
-function othervideoContract () {
-    $(this).clearQueue();
-    $('.innerText-').clearQueue().animate({'opacity': '0.0'}, 300);
-    $(this).delay( 500 ).animate({'height': '1em'}, 500);
+    $(this).delay( 500 ).animate({'height': '10em'}, 500);
     $(this).animate({'width': '10em'}, 500);                           
 }
+
 
 //category select
 
@@ -212,16 +165,11 @@ function allSelect (){
         $('#design').css("background-color", "transparent");
         $('#interactive').css("background-color", "transparent");
         playChord();
-      $('#tn-graphicdesign').clearQueue().animate({'width': '10em'}, 500);
-        $('#tn-float').clearQueue().animate({'width': '11em'}, 500);
-        $('#tn-hive').clearQueue().animate({'width': '10em'}, 500);
-        $('#tn-noisy').clearQueue().animate({'width': '12em'}, 500);
-        $('#tn-map').clearQueue().animate({'width': '9em'}, 500);
-        $('#tn-darkroom').clearQueue().animate({'width': '13em'}, 500);
-        $('#tn-surface').clearQueue().animate({'width': '14em'}, 500);
-        $('#tn-screws').clearQueue().animate({'width': '12em'}, 500);
-        $('#tn-othergame').clearQueue().animate({'width': '9em'}, 500);
-        $('#tn-othervideo').clearQueue().animate({'width': '10em'}, 500);
+          $('#tn-motiongraphics').clearQueue().animate({'width': '40em'}, 500);
+      $('#tn-graphicdesign').clearQueue().animate({'width': '40em'}, 500);
+        $('#tn-hive').clearQueue().animate({'width': '40em'}, 500);
+        $('#tn-stilllife').clearQueue().animate({'width': '40em'}, 500);
+        $('#tn-understated').clearQueue().animate({'width': '40em'}, 500);
 }
 function designSelect (){
     console.log("triggered");
@@ -230,18 +178,12 @@ function designSelect (){
         $('#sound').css("background-color", "transparent");
         
             playChord();
-    
-        $('#tn-hive').clearQueue().animate({'width': '0em'}, 500);
-        $('#tn-noisy').clearQueue().animate({'width': '0em'}, 500);
-        $('#tn-screws').clearQueue().animate({'width': '0em'}, 500);
-        $('#tn-othervideo').clearQueue().animate({'width': '0em'}, 500);
-        $('#tn-float').clearQueue().animate({'width': '0em'}, 500);
-    
-         $('#tn-graphicdesign').clearQueue().animate({'width': '10em'}, 500);
-        $('#tn-map').clearQueue().animate({'width': '9em'}, 500);
-        $('#tn-surface').clearQueue().animate({'width': '14em'}, 500);
-        $('#tn-darkroom').clearQueue().animate({'width': '13em'}, 500);
-        $('#tn-othergame').clearQueue().animate({'width': '9em'}, 500);
+            $('#tn-hive').clearQueue().animate({'width': '10em'}, 500);
+        $('#tn-stilllife').clearQueue().animate({'width': '10em'}, 500);
+
+              $('#tn-motiongraphics').clearQueue().animate({'width': '40em'}, 500);
+        $('#tn-understated').clearQueue().animate({'width': '40em'}, 500);
+            $('#tn-graphicdesign').clearQueue().animate({'width': '40em'}, 500);
     
 }
 function intSelect (){
@@ -250,18 +192,13 @@ function intSelect (){
         $('#design').css("background-color", "transparent");
         $('#sound').css("background-color", "transparent");
             playChord();
-        $('#tn-map').clearQueue().animate({'width': '0em'}, 500);
-        $('#tn-surface').clearQueue().animate({'width': '0em'}, 500);
-         $('#tn-screws').clearQueue().animate({'width': '0em'}, 500);
-        $('#tn-othervideo').clearQueue().animate({'width': '0em'}, 500);
-    $('#tn-graphicdesign').clearQueue().animate({'width': '0em'}, 500);
+              $('#tn-motiongraphics').clearQueue().animate({'width': '10em'}, 500);
+        $('#tn-understated').clearQueue().animate({'width': '10em'}, 500);
+    $('#tn-graphicdesign').clearQueue().animate({'width': '10em'}, 500);
     
     
-        $('#tn-hive').clearQueue().animate({'width': '10em'}, 500);
-        $('#tn-noisy').clearQueue().animate({'width': '12em'}, 500);
-        $('#tn-darkroom').clearQueue().animate({'width': '13em'}, 500);
-        $('#tn-othergame').clearQueue().animate({'width': '9em'}, 500);
-        $('#tn-float').clearQueue().animate({'width': '11em'}, 500);
+        $('#tn-hive').clearQueue().animate({'width': '40em'}, 500);
+        $('#tn-stilllife').clearQueue().animate({'width': '40em'}, 500);
 }
 function soundSelect (){
     console.log("triggered");
@@ -269,26 +206,19 @@ function soundSelect (){
         $('#design').css("background-color", "transparent");
         $('#interactive').css("background-color", "transparent");
             playChord();
-        $('#tn-darkroom').clearQueue().animate({'width': '0em'}, 500);
-        $('#tn-surface').clearQueue().animate({'width': '0em'}, 500);
-        $('#tn-othergame').clearQueue().animate({'width': '0em'}, 500);
-    $('#tn-graphicdesign').clearQueue().animate({'width': '0em'}, 500);
-    
-    
         $('#tn-hive').clearQueue().animate({'width': '10em'}, 500);
-        $('#tn-noisy').clearQueue().animate({'width': '12em'}, 500);
-        $('#tn-map').clearQueue().animate({'width': '9em'}, 500);
-        $('#tn-screws').clearQueue().animate({'width': '12em'}, 500);
-        $('#tn-othervideo').clearQueue().animate({'width': '10em'}, 500);
-         $('#tn-float').clearQueue().animate({'width': '11em'}, 500);
+        $('#tn-understated').clearQueue().animate({'width': '10em'}, 500);
+    
+              $('#tn-motiongraphics').clearQueue().animate({'width': '40em'}, 500);
+        $('#tn-graphicdesign').clearQueue().animate({'width': '40em'}, 500);
+        $('#tn-stilllife').clearQueue().animate({'width': '40em'}, 500);
+
 }
 
 //load page
 
 
 $(document).ready(function(){
-    
-//    $('.hey').hover(playAudio);
     
     $('#allWork').click(allSelect);
     $('#design').click(designSelect);
@@ -297,28 +227,6 @@ $(document).ready(function(){
     
     $('.upperText').css('display', 'none');
     $('.upperText').fadeIn(1000); 
-    
-        $('#tn-graphicdesign').hover(graphicdesignExpand, graphicdesignContract);
-      $('#tn-float').hover(floatExpand, floatContract);
-    $('#tn-hive').hover(hiveExpand, hiveContract);
-    $('#tn-noisy').hover(noisyExpand, noisyContract);
-    $('#tn-map').hover(mapExpand, mapContract);
-    $('#tn-darkroom').hover(darkroomExpand, darkroomContract);
-    $('#tn-surface').hover(surfaceExpand, surfaceContract);
-     $('#tn-screws').hover(screwsExpand, screwsContract);
-     $('#tn-othergame').hover(otherExpand, othergameContract);
-     $('#tn-othervideo').hover(otherExpand, othervideoContract);
-    
-    
-//        $('#tn-hive').hover(expand, hiveContract);
-//    $('#tn-noisy').hover(expand, noisyContract);
-//    $('#tn-map').hover(expand, mapContract);
-//    $('#tn-darkroom').hover(expand, darkroomContract);
-//    $('#tn-surface').hover(expand, surfaceContract);
-//     $('#tn-screws').hover(expand, screwsContract);
-//     $('#tn-othergame').hover(expand, othergameContract);
-//     $('#tn-othervideo').hover(expand, othervideoContract);
-//      $('#tn-float').hover(expand, floatContract);
     
     $('.link').click(function(event) {
         event.preventDefault();
